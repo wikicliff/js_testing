b3b5a36acb96005268870b63d7c4a9d8611711fa
Clifford Adams <wikicliff@gmail.com>
2013-06-25 23:21:52 -0400

    moment_js folder added

---
+++ moment_js/public_html/test_moment.js
@@ -0,0 +1,11 @@
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+console.log("Starting tests.");
+console.log(moment().millisecond());
+console.log(moment().valueOf());
+console.log(moment().valueOf());
+console.log(moment().valueOf());
+console.log("End of tests.");
